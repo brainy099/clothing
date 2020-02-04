17a1f304edd2e3008fac0f06a6de76ef1750f72b
Brainy Maibam <brainy099@gmail.com>
2020-02-04 12:14:55 +0530

    added routes with withRouter HOC

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from "react";
 import "./App.css";
 import Homepage from "./pages/homepage/homepage.component";
+import { Route } from "react-router-dom";
+
+const HatsPage = () => <div>Hats</div>;
 
 function App() {
   return (
     <div>
-      <Homepage />
+      <Route exact path="/" component={Homepage} />
+      <Route path="/hats" component={HatsPage} />
     </div>
   );
 }
