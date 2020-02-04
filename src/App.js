86e1a2481c8b346a0886f5a4e2d65a3cce634357
Brainy Maibam <brainy099@gmail.com>
2020-02-04 22:43:21 +0530

    added sign in form

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import "./App.css";
 import Homepage from "./pages/homepage/homepage.component";
 import Header from "./components/header/header.component";
 import ShopPage from "./pages/shop/shop.component";
+import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
 import { Route, Switch } from "react-router-dom";
 
 const HatsPage = () => <div>Hats</div>;
@@ -14,6 +15,7 @@ function App() {
       <Switch>
         <Route exact path="/" component={Homepage} />
         <Route path="/shop" component={ShopPage} />
+        <Route path="/signin" component={SignInAndSignUpPage} />
       </Switch>
     </div>
   );
