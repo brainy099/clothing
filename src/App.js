51325da11ba8efa1b2182d4af62d1e8222f640a3
Brainy Maibam <brainy099@gmail.com>
2020-02-04 17:57:59 +0530

    header component, links to shop, home and contacts added

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 import React from "react";
 import "./App.css";
 import Homepage from "./pages/homepage/homepage.component";
+import Header from "./components/header/header.component";
 import ShopPage from "./pages/shop/shop.component";
-import { Route } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 
 const HatsPage = () => <div>Hats</div>;
 
 function App() {
   return (
     <div>
-      <Route exact path="/" component={Homepage} />
-      <Route path="/shop" component={ShopPage} />
+      <Header />
+      <Switch>
+        <Route exact path="/" component={Homepage} />
+        <Route path="/shop" component={ShopPage} />
+      </Switch>
     </div>
   );
 }
