4a3fe06f24e31d24d8b6930052b195956da6a4a0
Brainy Maibam <brainy099@gmail.com>
2020-02-04 16:28:44 +0530

    added shop page, collection-preview and collection item component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./App.css";
 import Homepage from "./pages/homepage/homepage.component";
+import ShopPage from "./pages/shop/shop.component";
 import { Route } from "react-router-dom";
 
 const HatsPage = () => <div>Hats</div>;
@@ -9,7 +10,7 @@ function App() {
   return (
     <div>
       <Route exact path="/" component={Homepage} />
-      <Route path="/hats" component={HatsPage} />
+      <Route path="/shop" component={ShopPage} />
     </div>
   );
 }
