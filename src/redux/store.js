8127116ace211306d9a15a6ed06b85e48fba2a8d
Brainy Maibam <brainy099@gmail.com>
2020-03-28 00:30:53 +0530

    added sagas, changed shop component pattern

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,16 +1,23 @@
 import { createStore, applyMiddleware } from "redux";
 import { persistStore } from "redux-persist";
 import logger from "redux-logger";
+import createSagaMiddleware from "redux-saga";
 
 import rootReducer from "./root-reducer";
 
-const middleWares = [];
+import { fetchCollectionsStart } from "./shop/shop.saga";
+
+const sagaMiddleware = createSagaMiddleware();
+
+const middleWares = [sagaMiddleware];
 
 if (process.env.NODE_ENV === "development") {
   middleWares.push(logger);
 }
 
 export const store = createStore(rootReducer, applyMiddleware(...middleWares));
+
+sagaMiddleware.run(fetchCollectionsStart);
 
 export const persistor = persistStore(store);
 
